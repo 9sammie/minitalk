b8926437a3197696fd1a825acc120d7a308d2325
Mathis Ballet <maballet@student.42lyon.fr>
2025-03-28 18:47:13 +0100

    yolo

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 16:22:26 by maballet          #+#    #+#             */
-/*   Updated: 2025/03/27 10:48:14 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/03/28 18:46:55 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,45 @@
 
 int	g_signal_received = 0;
 
+void	signal_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		g_signal_received = 1;
+	if (signum == SIGUSR2)
+		ft_printf_fd(1, "Message entirely received by the server 🏝️\n");
+}
+
+void	send_char(pid_t pid, unsigned char c)
+{
+	int	bit_index;
+	int	time_out;
+
+	bit_index = 0;
+	while (bit_index < 8)
+	{
+		g_signal_received = 0;
+		if ((c &(1 << bit_index)) != 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		while (g_signal_received == 0)
+		{
+			time_out++;
+			pause();
+		}
+		if (time_out >= 100)
+		{
+			ft_printf_fd(1, "No news from the server 💔\n");
+			return ;
+		}
+		bit_index++;
+	}
+}
+
 void	send_len(pid_t pid, int len)
 {
 	int	bit_index;
+	int	time_out;
 
 	bit_index = 0;
 	while (bit_index < 32)
@@ -26,10 +62,47 @@ void	send_len(pid_t pid, int len)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		while (g_signal_received == 0)
+		while (g_signal_received == 0 && time_out < 100)
+		{
+			time_out++;
 			pause();
+		}
+		if (time_out >= 100)
+		{
+			ft_printf_fd(1, "No news from the server 💔\n");
+			return ;
+		}
 		bit_index++;
 	}
+}
+
+void	send_message(pid_t pid, char *message)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(message);
+	send_len(pid, len);
+	while (message[i])
+	{
+		send_char(pid, message[i]);
+		i++;
+	}
+}
+
+int	sigaction_init(void)
+{
+	struct sigaction	sa;
+	
+	ft_memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	return (0);
 }
 
 int	param_check(int argc, char**argv)
@@ -46,72 +119,17 @@ int	param_check(int argc, char**argv)
 	{
 		if (!ft_isdigit(argv[1][i]))
 		{
-			ft_printf_fd(2,"Invalid PID\n");
+			ft_printf_fd(2,"Invalid PID 🛟\n");
 			return (1);	
 		}
 		i++;
 	}
 	if (argv[2][0] == 0)
 	{
-		ft_printf_fd(2, "Message missing");
+		ft_printf_fd(2, "Message missing 🛟\n");
 		return (1);
 	}
 	return (0);
-}
-
-void	signal_handler(int signum)
-{
-	if (signum == SIGUSR1)
-		g_signal_received = 1;
-}
-
-int	sigaction_init(void)
-{
-	struct sigaction	sa;
-	
-	ft_memset(&sa, 0, sizeof(sa));
-	sa.sa_handler = signal_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	return (0);
-}
-
-void	send_char(pid_t pid, unsigned char c)
-{
-	int	bit_index;
-
-	bit_index = 0;
-	while (bit_index < 8)
-	{
-		g_signal_received = 0;
-		if ((c &(1 << bit_index)) != 0)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		while (g_signal_received == 0)
-			pause();
-		bit_index++;
-	}
-}
-
-void	send_message(pid_t pid, char *message)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = ft_strlen(message);
-	send_len(pid, len);
-	while (message[i])
-	{
-		//ft_printf_fd(1, "sent char : %c\n", message[i]);
-		send_char(pid, message[i]);
-		i++;
-	}
-	send_char(pid, '\0');
-	ft_printf_fd(1, "Message sent successfully!\n");
 }
 
 int	main(int argc, char **argv)
@@ -129,7 +147,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	message = argv[2];
-	ft_printf_fd(1, "Sending message to server (PID: %d)...\n", server_pid);
+	ft_printf_fd(1, "Sending message to the server 🏄 (PID: %d)...\n", server_pid);
 	send_message(server_pid, message);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 21:40:48 by maballet          #+#    #+#             */
-/*   Updated: 2025/03/27 11:20:14 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/03/28 17:58:05 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,35 @@
 
 int	str_index = 0;
 
-int	get_strlen(int signum, siginfo_t *info, void *context)
+int	get_strlen(int client_pid, int signum)
 {
     static int		bit_count = 0;
-    static pid_t	client_pid = 0;
-	static int		len;
+	static int		len = 0;
 
-    (void)context;
 	if (bit_count == 0)
-	{
 		len = 0;
-		client_pid = 0;
-	}
-    if (!client_pid)
-        client_pid = info->si_pid;
     if (signum == SIGUSR2)
         len |= (1 << bit_count);
     kill(client_pid, SIGUSR1);
     bit_count++;
-    if (bit_count == 32)
-    {
-		ft_printf_fd(1, "len %d\n", len);
-        bit_count = 0;
+    if (bit_count >= 32)
+	{
+		bit_count = 0;
         return (len);
-    }
+	}
 	return (0);
 }
 
-char	*store_str(char *str, int signum, siginfo_t *info, void *context)
+char	*store_str(int client_pid, char *str, int signum)
 {
 	static unsigned char	c = 0;
 	static int				bit_count = 0;
-	static pid_t			client_pid = 0;
 
-	(void)context;
-	if (str_index == 0 && bit_count == 0)
-		client_pid = 0;
-	if (!client_pid)
-		client_pid = info->si_pid;
-	if (bit_count == 0)
-		c = 0;
 	if (signum == SIGUSR2)
 		c |= (1 << bit_count);
 	kill(client_pid, SIGUSR1);
 	bit_count++;
-	if (bit_count == 8)
+	if (bit_count >= 8)
 	{
 		str[str_index++] = c;
 		c = 0;
@@ -67,19 +51,19 @@ char	*store_str(char *str, int signum, siginfo_t *info, void *context)
 	return (str);
 }
 
-void	print_str(int len, char **str)
+void	print_str(int len, char **str, int *receiving_len)
 {
 	static int	received_bits = 0;
 
 	received_bits++;
-	if (received_bits == len * 8)
+	if (received_bits >= len * 8)
 	{
-		ft_printf_fd(1, "%s\n", str);
-		free(str);
-		str = NULL;
-		len = 0;
+		ft_printf_fd(1, "%s\n", *str);
+		free(*str);
+		*str = NULL;
 		received_bits = 0;
 		str_index = 0;
+		*receiving_len = 1;
 	}
 }
 
@@ -88,12 +72,13 @@ void	signal_handler(int signum, siginfo_t *info, void *context)
 	static int		len = 0;
 	static char		*str = NULL;
 	static int		receiving_len = 1;
-	static int		received_bits = 0;
+	int				client_pid;
 
+	(void)context;
+	client_pid = info->si_pid;
 	if (receiving_len == 1)
 	{
-		len = 0;
-		len = get_strlen(signum, info, context);
+		len = get_strlen(client_pid, signum);
 		if (len > 0)
 		{
 			receiving_len = 0;
@@ -105,9 +90,13 @@ void	signal_handler(int signum, siginfo_t *info, void *context)
 		}
 		return ;
 	}
-	str = store_str(str, signum, info, context);
-	print_str(len, str);
-	receiving_len = 1;
+	str = store_str(client_pid, str, signum);
+	print_str(len, &str, &receiving_len);
+	if (receiving_len == 1)
+	{
+		kill(client_pid, SIGUSR2);
+		len = 0;
+	}
 }
 
 int	sigaction_init(void)
@@ -116,6 +105,7 @@ int	sigaction_init(void)
 	
 	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_sigaction = signal_handler;
+	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 	{
